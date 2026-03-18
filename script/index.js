b7ad0e157d462a07832ca3bbe9537878cda32245
tildanyrvik <tilda.nyrvik@gmail.com>
2026-03-18 18:56:44 +0100

    arithmetic operations calculated

--- script/index.js
+++ script/index.js
@@ -10,3 +10,12 @@ age = 27;
 color = "green";
 
 console.log(`My name is ${name} I am ${age} years old, my favorite color is ${color}.`);
+
+
+let x = 10;
+let y = 20;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
