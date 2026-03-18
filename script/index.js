82348556f5116ef69834e1b59275cde1624f834b
tildanyrvik <tilda.nyrvik@gmail.com>
2026-03-18 18:42:13 +0100

    variables created and assigned

---
+++ script/index.js
@@ -0,0 +1,12 @@
+
+console.log("Hello world");
+
+let name;
+let age;
+let color;
+
+name = "Tilda";
+age = 27;
+color = "green";
+
+console.log(`My name is ${name} I am ${age} years old, my favorite color is ${color}.`);
